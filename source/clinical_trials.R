dc1c6581791126077c6adad193715b9220869a9e
Sergio <sergio.alonso.phd@gmail.com>
2025-03-28 16:47:05 +0100

    Added files to analyse PubChem compounds

--- source/clinical_trials.R
+++ source/clinical_trials.R
@@ -6,35 +6,17 @@ library(RColorBrewer)
 library(gridExtra)
 library(igraph)
 library(stringr)
-
+library(httr)
+library(editData)
+library(googlesheets4)
 
 # useful functions to expand and contract terms
 
 join1 <- function(x) paste(x,collapse="|")
-split1 <- function(x) strsplit(x,split="\\|")
-
-# ggplot theme
-
-palette <- list()
-
-palette$msi <- c(MSS="#BCBDDC",MSI="#6A51A3","MSI/HYPER"="#6A51A3")
-palette$mut <- c(WT="#FEE6CE",MUT="#F16913")
-palette$study <- c("#C19F08","#369E4B","#357EB9","#FA792F","#ce3206")
-names(palette$study) <- c("ALL","DFCI Primary","TCGA Primary","MSKCC Primary","MSKCC Metastasis")
-palette$study2 <- brewer.pal(9,"Greens")[c(3,5:9)]
-names(palette$study2) <- c("ALL","DFCI Primary","TCGA Primary","MSKCC Primary","MSKCC Metastasis")
-
-palette$stage <- brewer.pal(9,"Blues")[c(2,4,6,8)]
-names(palette$stage) <- c("I","II","III","IV")
-palette$sample.type <- c("Primary"="#8EBEDA","Metastasis"="#2478B1")
-
-mytheme <- theme(panel.background = element_rect(fill="grey95"),
-                 strip.background = element_rect(fill="grey90",colour="grey30"),
-                 panel.grid = element_line(color="white"),
-                 panel.border = element_rect(color="grey30",fill=NA,linewidth=rel(1)),
-) 
+split1 <- function(x) strsplit(x,split="\\|") 
 
 
+# read the Thesaurus database
 
 if(!file.exists("data/Thesaurus.txt")) {
   
@@ -42,12 +24,15 @@ if(!file.exists("data/Thesaurus.txt")) {
   
 }
 
-
-thesaurus <- fread("data/Thesaurus.FLAT.zip",quote="")
+thesaurus <- fread("data/Thesaurus.zip",quote="")
 names(thesaurus) <- c("code","concept IRI","parents","synonyms","definition","display name","concept status","semantic type","concept in subset")
 thesaurus$synonyms %>% split1 %>% sapply(FUN=function(x) x[1]) -> thesaurus$name # the preferred name
 setkey(thesaurus,"code")
-
+tsearch <- function(x) {
+  a <- thesaurus[tolower(x)==tolower(name)]
+  if(nrow(a)==0) a <- thesaurus[grep(x,synonyms,ignore.case = T)]
+  return(a)
+}
 # remove retired concepts
 
 thesaurus <- thesaurus[!grepl("Retired",`concept status`)]
@@ -150,8 +135,6 @@ drugs[duplicated(code)]
 drugs[,named_mutations:=str_extract_all(paste(synonyms,definition) %>% toupper,"(G[0-9]+[A-Z]|V600[A-Z]|BRAF|B-RAF|PAN-[K]*RAS|PAN-[B]*RAF)")[[1]] %>% 
         gsub("B-RAF","BRAF",.) %>% unique %>% paste(.,collapse=":"),by=code]
 
-
-
 drugs$Target <- NULL
 
 #G12C
@@ -168,7 +151,9 @@ drugs[grep("G12D",synonyms),Target:="G12D"]
 drugs[grep("G12D",paste(synonyms,definition),ignore.case = T)]
 
 #SOS1/SHP2
-drugs[grep("SOS1|SHP2",Group),Target:="SOS1:SHP2"]
+drugs[grep("SOS1",Group),Target:="SOS1"]
+drugs[grep("SHP2",Group),Target:="SHP2"]
+
 
 #BRAF
 drugs[grep("BRAF",Group),Target:="BRAF"]
@@ -190,6 +175,8 @@ drugs[grep("Urea",name),`:=`(synonyms=paste0(synonyms,"|heterocyclic urea"),Type
 # remove some salt forms of drugs
 remove <- "Divarasib Adipate|Rigosertib Sodium|Sorafenib Tosylate|Dabrafenib Mesylate|Lifirafenib Maleate|Regorafenib Anhydrous"
 drugs <- drugs[grep(remove,name,invert = T)]
+
+drugs[grep("ZG2001",synonyms),synonyms:=paste0(synonyms,"|ZG2001|ZG-2001")]
 
 
 # correct paluratide = LUNA18
@@ -220,7 +207,6 @@ drugs[code == "C29136",Target:="G13N"]
 # Pan-Ras
 
 drugs[is.na(Target),list(name,definition,code)] -> x
-
 drugs[code %in% c("C178264","C185876","C188048","C71146","C209714","C213206"),Target:="pan-RAS"]
 
 
@@ -238,55 +224,70 @@ drugs <- merge(drugs,fread("additional_files/short.names.csv")[,list(code,short.
 
 # Note: it is unclear whether LY4066434 is G12D or pan-KRAS
 
-additional.drugs <- fread("additional_files/drug.revision.csv")
+additional.drugs <- fread("additional_files/manually_added.csv")
+additional.drugs[Code=="",Code:="NA"]
 
-intersect(additional.drugs$synonyms %>% strsplit(.,split="\\|") %>% unlist(),
-          drugs$synonyms %>% strsplit(.,split="\\|") %>% unlist()) %>% paste(.,collapse="|") -> already.included
+names(drugs)
+names(additional.drugs) <- c("code","From","name","short.Name","synonyms","Target","Group","Type")
 
-additional.drugs[,`:=`(From="Manually Added",children=0)]
+drugs[! code %in% additional.drugs$code,list(code,Group)]
 
-drugs <- rbind(drugs,additional.drugs,fill=T)
-
-
+drugs <- rbind(drugs,additional.drugs[From=="Manually Added"],fill=T)
 
 # end creating the drugs data.table -------
 
 drugs[,table(From,Group)]
-drugs[is.na(code),code:="N.A."]
-
-x <- drugs[Target!="BRAF"][order(Target,Type),list("Thesaurus\ncode"=code,"Name"=short.Name,Target,Type)]
+x <- drugs[Target!="BRAF"][order(Target,Type),list("NCI\ncode"=code,"Name"=short.Name,Target,Type)]
 dim(x)
-png("output/drugs_table.png",1200,1400)
-g1 <- tableGrob(x[1:45],rows=NULL,theme=ttheme_default(base_size=20))
-g2 <- tableGrob(x[46:90],rows=NULL,theme=ttheme_default(base_size=20))
+png("output/drugs_table.png",1200,1600)
+g1 <- tableGrob(x[1:52],rows=NULL,theme=ttheme_default(base_size=20))
+g2 <- tableGrob(x[53:103],rows=NULL,theme=ttheme_default(base_size=20))
 grid.arrange(g1,g2,layout_matrix=matrix(1:2,ncol=2))
 dev.off()
 
 # Clinical trials for colorectal cancer or solid tumors ----
-# I haven't found a more elegant way of downloading the clincal trials data
-# Downloading clinical trials data requires the input from the user...
 
-browseURL("https://clinicaltrials.gov/search?cond=((colon%20OR%20rectal%20OR%20colorectal)%20AND%20cancer)%20OR%20(solid%20tumors)")
-# download ctg-studies 
-file.copy(from = "~/Downloads/ctg-studies.csv",to = "additional_files/ctg-studies.csv",overwrite = T)
+if(!file.exists("data/ctg-studies.csv")) {
+  
+  url_base <- "https://clinicaltrials.gov/api/v2/studies?"
+  url_query <- "query.cond=((colon+OR+rectal+OR+colorectal+OR+CRC)+AND+cancer)+OR+(solid+tumors)"
+  query_url <- paste0(url_base,url_query,"&format=csv&pageSize=500")
+  
+  message("Downloading registries 1 to 500")
+  n <- 0
+  query <- GET(query_url)
+  ctrials <- fread(content(query,as="text",encoding = "UTF-8"))
+  colnames_trials <- colnames(ctrials)
+  nextToken <- query$headers$`x-next-page-token`
+  
+  while(!is.null(nextToken)) {
+    n <- n + 1
+    message("Downloading registries ",500*n+1," to ",500*(n+1))
+    query_url <- paste0(url_base,url_query,"&format=csv&pageSize=500&pageToken=",nextToken)
+    query <- GET(query_url)
+    x <- fread(content(query,as="text",encoding = "UTF-8"))
+    colnames(x) <- colnames_trials
+    ctrials <- rbind(ctrials,x)
+    nextToken <- query$headers$`x-next-page-token`
+  }
+  
+  fwrite(ctrials,file="data/ctg-studies.csv")
+  
+}
 
-ctrials <- fread("additional_files/ctg-studies.csv")
+ctrials <- fread("data/ctg-studies.csv")
 setkey(ctrials,"NCT Number")
 
 # correct some misspellings
-ctrials[grep("LY4066434.",Interventions),Interventions:=gsub("LY4066434\\.","LY4066434",Interventions)] 
+
 ctrials[grep("5Fluoro",Interventions,ignore.case = T),Interventions:=gsub("5Fluoro","5-Fluoro",Interventions,ignore.case = T)] 
 
-message("Loaded ",dim(ctrials)[1]," clinical trials") 
-
-
-# search in clinical trials
+message("Loaded ",nrow(ctrials)," clinical trials") 
 
 # Search for clinical trials on CRC (or gastrointestinal) cancers
 
 crc <- ctrials[grepl("colon|rectal|gastrointestinal|crc|solid",Conditions,ignore.case = T) & `Study Type`=="INTERVENTIONAL"]
 crc <- crc[grep("TREATMENT",`Study Design`)]
-crc <- rbind(crc,ctrials[grep("NCT06385925|NCT04330664|NCT03785249",`NCT Number`)])
 
 drug.columns <- c("Drug.Code","Drug.Name","Drug.Type","Drug.Target","Drug.Group")
 crc[,(drug.columns) := ""]
@@ -303,12 +304,24 @@ for(i in 1:nrow(drugs)) {
 # remove the initial |
 
 crc[,(drug.columns) := lapply(.SD,function(x) gsub("^\\|","",x)),.SDcols=drug.columns]
-
 dim(crc)
+
+# select trials targeting KRAS, SHP2 or SOS1
 
 kras.trials <- crc[grep("KRAS|SHP2|SOS1",Drug.Group)]
 message("KRAS Trials n=",nrow(kras.trials))
 
+# trials in review 2024
+inReview <- c("NCT05485974","NCT05462717","NCT06244771","NCT04121286","NCT06385925","NCT05737706","NCT06364696","NCT06403735","NCT06040541","NCT06412198","NCT05194995","NCT04330664","NCT05288205","NCT06039384","NCT05198934","NCT04956640","NCT05578092","NCT04699188","NCT04449874","NCT05358249","NCT06026410","NCT06252649","NCT06586515","NCT04793958","NCT03785249","NCT05722327","NCT06599502","NCT06078800","NCT06607185","NCT06447662","NCT06585488","NCT06445062","NCT05379985","NCT05786924","NCT06194877","NCT05200442","NCT06270082","NCT05163028","NCT04117087","NCT04853017","NCT06411691","NCT05726864","NCT06105021","NCT06253520","NCT06487377","NCT06218914")
+inReview <- ctrials[inReview]
+
+setdiff(inReview$`NCT Number`,crc[Drug.Type!="",`NCT Number`]) %>% ctrials[.] -> missed
+
+message(nrow(missed)," clinical trials not retrieved by the automatic method, manually added")
+
+kras.trials <- rbind(kras.trials,missed,fill=TRUE)
+
+# reassign the drug information using only drugs targeting KRAS, SOS1 or SHP2
 kras.trials[,(drug.columns) := ""]
 drugs2 <- drugs[Target!="BRAF"]
 
@@ -323,49 +336,75 @@ for(i in 1:nrow(drugs2)) {
 
 kras.trials[,(drug.columns) := lapply(.SD,function(x) gsub("^\\|","",x)),.SDcols=drug.columns]
 
-kras.trials[Drug.Target!=""]
-
-# trials in review 2024
-inReview <- c("NCT05485974","NCT05462717","NCT06244771","NCT04121286","NCT06385925","NCT05737706","NCT06364696","NCT06403735","NCT06040541","NCT06412198","NCT05194995","NCT04330664","NCT05288205","NCT06039384","NCT05198934","NCT04956640","NCT05578092","NCT04699188","NCT04449874","NCT05358249","NCT06026410","NCT06252649","NCT06586515","NCT04793958","NCT03785249","NCT05722327","NCT06599502","NCT06078800","NCT06607185","NCT06447662","NCT06585488","NCT06445062","NCT05379985","NCT05786924","NCT06194877","NCT05200442","NCT06270082","NCT05163028","NCT04117087","NCT04853017","NCT06411691","NCT05726864","NCT06105021","NCT06253520","NCT06487377","NCT06218914")
-inReview <- ctrials[inReview]
-
-setdiff(inReview$`NCT Number`,crc[Drug.Type!="",`NCT Number`]) %>% ctrials[.] -> missed
-dim(missed) # two trials were not retrieved by using the previous approach
+kras.trials[Drug.Target=="",list(`NCT Number`,Interventions)]
 
 # check missed trials
 
-x <- ctrials[grepl("k[-]*ras",Interventions,ignore.case = T) & ! `NCT Number` %in% kras.trials$`NCT Number` &
+x <- ctrials[grepl("k[-]*ras",paste(Interventions),ignore.case = T) & ! `NCT Number` %in% kras.trials$`NCT Number` &
                grepl("TREATMENT",`Study Design`)]
-dim(x) # 7 trials putatively missed
-
-# Targeting KRAS: 1,2,4,5,6,7
-
-x <- rbind(kras.trials,x[c(1,2,4,5,6,7)],fill=T)
-x2 <- fread("data/curated.kras.trials.csv") # manually curated
-
-x$`NCT Number` == x2$`NCT Number`
-
-x$Drug.Code <- x2$Drug.Code
-x$Drug.Name <- x2$Drug.Name
-x$Drug.Type <- x2$Drug.Type
-x$Drug.Target <- x2$Drug.Target
-x$Drug.Group <- x2$Drug.Group
-
-kras.trials <- x
-rm(x,x2)
-
-table(kras.trials$Drug.Type)
-table(kras.trials$Drug.Target)
-# review trials with more than one identified drug
 
 
-# save for manual revision 
+x[,list(`NCT Number`,Interventions)] # NCT04745130 is not targeting KRAS: Sintilimab，regofinib，cetuximab
 
-# fwrite(kras.trials,file="data/curated.kras.trials.csv")
+x <- x[`NCT Number`!="NCT04745130"]
+
+dim(x)
+
+kras.trials <- rbind(kras.trials,x,fill=T)
+
+# all the selected KRAS-related drugs
+
+x <- kras.trials
+x[,Synonyms:=drugs2[match(split1(Drug.Code) %>% unlist,code),synonyms %>% join1],by=`NCT Number`]
+x$Synonyms
+
+
+# Checking other possible missed trials
+
+crc[grepl("k[-]*ras",paste(`Brief Summary`),ignore.case = T) & 
+      !(`NCT Number` %in% kras.trials$`NCT Number`) &
+      !grepl("wild[ -]*type",`Brief Summary`,ignore.case = T)] -> toReview
+
+fwrite(drugs2[,list(Code=code,From,Name=name,"Short Name"=short.Name,synonyms,Target,Group,Type)],file="additional_files/drugs.kras.csv")
+
+lapply(drugs2$synonyms %>% strsplit(split="\\|") %>% unlist %>% toupper %>% unique,function(i) {
+  print(i)
+  drug <- drugs2[grep(i,synonyms,ignore.case = T),]
+  selected <- kras.trials[grep(i,Interventions,ignore.case = T),list(`NCT Number`)]
+  data.table("Identified term"=i,drug[,list("Drug Name"=name,Target,Type,Group)],selected)
+}) %>% rbindlist %>% na.omit -> foo
+
+merge(kras.trials[,list(`NCT Number`,`Study Title`,`Study URL`,Phases,Interventions)],foo,all.x=T) -> foo
+foo[,`Identified term` := sort(`Identified term`)[1],by=list(`NCT Number`,`Drug Name`)]
+foo <- unique(foo)
+
+fwrite(foo,file="additional_files/included.csv")
+
+toReview[,strsplit(Interventions,split="(: |\\|)") %>% unlist %>% gsub("(DRUG|PROCEDURE|  )","",.) %>% paste(collapse=", "),by=`NCT Number`]
+
+fwrite(toReview[,list(Include="TBD",`NCT Number`,`Study Title`,Interventions,`Study URL`)],file = "additional_files/toReview.csv")
+
+
+
+# manually review the clinical trial table, to verify the information automatically 
+# added and the last 6 clinical trials added
+
+fwrite(kras.trials[,list(`NCT Number`,`Study Title`,Acronym,Interventions,Drug.Code,Drug.Name,Drug.Target,Drug.Group)],
+       file="data/toReview.kras.trials.csv")
+
+# 
+
+
+message("Please review ")
 
 # fread the revised version
 
 try(kras.trials <- fread("data/curated.kras.trials.csv"),silent=F)
+kras.trials <- merge(ctrials,kras.trials[,list(`NCT Number`,Drug.Code,Drug.Name,Drug.Target,Drug.Group)])
+
+# fix Acronyms for Phase III studies
+
+kras.trials[match(c("NCT04793958","NCT05198934","NCT6252649"),`NCT Number`),Acronym:=c("KRYSTAL-10","CodeBreaK 300","CodeBreaK 301")]
 
 # add bibligraphic information (takes several min)
 
@@ -396,38 +435,46 @@ try(kras.trials <- fread("data/curated.kras.trials.csv"),silent=F)
 kras.trials[nchar(`Start Date`) > 8,start:=as.Date(`Start Date`)]
 kras.trials[nchar(`Start Date`) == 7,start:=as.Date(paste0(`Start Date`,"-01"))]
 
-kras.trials[nchar(`Completion Date`) > 8,completion:=as.Date(`Completion Date`)]
-kras.trials[nchar(`Completion Date`) == 7,completion:=as.Date(paste0(`Completion Date`,"-01"))]
+kras.trials[nchar(`Primary Completion Date`) > 8,completion:=as.Date(`Primary Completion Date`)]
+kras.trials[nchar(`Primary Completion Date`) == 7,completion:=as.Date(paste0(`Primary Completion Date`,"-01"))]
 kras.trials[is.na(completion)]
 
 my.palette <- list()
-my.palette$kras.type <- c("#3588C7","#1A4EA0","#8FBA6F","#4F8B7A","#405D02","#F8CB1C","#C65684","#EF0060")
-
-table(kras.trials$Phases)
+my.palette$study.status <- c(TERMINATED="#9b2226",
+                             COMPLETED="#E73D23",
+                             UNKNOWN="#555555",
+                             ACTIVE_NOT_RECRUITING="#438BC7",
+                             RECRUITING="#3AA691",
+                             ENROLLING_BY_INVITATION="#59C5AF",
+                             NOT_YET_RECRUITING="#95DACC")
 
 
 x <- kras.trials
 x[completion > `Last Update Posted`,last:=completion]
 x[completion <= `Last Update Posted`,last:=`Last Update Posted`]
 x[,target := Drug.Target %>% split1 %>% unlist %>% sort %>% unique %>% join1,by=`NCT Number`]
-x <- x[order(target,start)]
+x <- x[order(Drug.Target,start)]
 x[,`NCT Number`:=factor(`NCT Number`,levels=as.character(`NCT Number`))]
 
-
+x$`Study Status` <- factor(x$`Study Status`,names(my.palette$study.status))
 
 trial.plot <- list(
   aes(start,`NCT Number`),
-  geom_vline(xintercept = as.Date("2025-03-15"),lwd=3,color="grey"),
-  geom_segment(aes(yend=`NCT Number`,xend=completion),lwd=2),
-  geom_point(aes(color=`Study Status`)),
+  geom_vline(xintercept = as.Date("2025-03-15"),lwd=3,color="lightgrey"),
+  geom_segment(aes(yend=`NCT Number`,xend=`Last Update Posted`),lwd=0.5,lty=5),
+  geom_segment(aes(yend=`NCT Number`,xend=completion,color=`Study Status`),lwd=2,show.legend=T),
   xlab(""),
   ylab(""),
-  scale_x_date(limits=as.Date(c("2010-01-01","2045-12-31"))),
+  scale_x_date(limits=as.Date(c("2012-01-01","2045-12-31"))),
   scale_y_discrete(limits=rev),
-  geom_point(aes(x=as.Date(`Last Update Posted`)),pch=21,lwd=0.2,fill="white"),
-  geom_text(aes(label=target,x=start),hjust=1,nudge_x=-365,size=3),
-  theme(strip.text.x.top = element_text(size=12,face = "bold"),
-        panel.background = element_rect(fill="white"))
+  geom_point(aes(x=as.Date(`Last Update Posted`)),pch=21,fill="white"),
+  geom_text(aes(label=target,x=start),hjust=1,nudge_x=-60,size=3),
+  scale_color_manual("Study Status",values=my.palette$study.status,drop=F),
+  theme(strip.text.x.top = element_text(size=16,face = "bold"),
+        panel.background = element_rect(fill="white",color="black"),
+        panel.grid = element_line(color="#EFEFEF"),
+        strip.background = element_rect(color="black"),
+        legend.key.height = unit(10, "pt"))
   
 )
 
@@ -435,19 +482,39 @@ active <- ! x$`Study Status` %in% c("COMPLETED","TERMINATED")
 
 g0 <- ggplot(x[!active]) + trial.plot +
   facet_wrap(~"Completed or terminated") +   
-  scale_x_date(limits=as.Date(c("2006-01-01","2030-12-31"))) +
-  theme(legend.position = "none")
+  scale_x_date(limits=as.Date(c("2000-01-01","2035-12-31"))) +
+  theme(legend.position = "none") +
+  geom_text(aes(label=sprintf("%s",Drug.Name),x=last),hjust=0,nudge_x = 240,size=3.2) 
+
+g1 <- ggplot(x[active & Phases=="PHASE1"]) + trial.plot + facet_wrap(~ "Phase I") + 
+  theme(legend.position = "none") +
+  geom_text(aes(label=sprintf("%s",Drug.Name),x=last),hjust=0,nudge_x = 60,size=3.2) 
 
 
-g1 <- ggplot(x[active & Phases=="PHASE1"]) + trial.plot + facet_wrap(~ "Phase I") + theme(legend.position = "none")
+g2 <- ggplot(x[active & grepl("PHASE2",Phases)]) + trial.plot + facet_wrap(~ "Phase I/II or II") + 
+  theme(legend.position = "none") +
+  geom_text(aes(label=sprintf("%s",Drug.Name),x=last),hjust=0,nudge_x = 60,size=3.2) 
 
-
-g2 <- ggplot(x[active & grepl("PHASE2",Phases)]) + trial.plot + facet_wrap(~ "Phase I/II or II") + theme(legend.position = "none")
 
 g3 <- ggplot(x[active & Phases=="PHASE3"]) + trial.plot + facet_wrap(~ "Phase III") + 
-  geom_text(aes(label=sprintf("%s (%s)",Acronym,Drug.Name),x=last),hjust=0,nudge_x = 365) 
+  geom_text(aes(label=sprintf("%s (%s)",Drug.Name,Acronym),x=last),hjust=0,nudge_x = 60)  
 
-grid.arrange(g0,g1,g2,g3,layout_matrix=matrix(c(2,3,1,2,4,4),byrow=T,ncol=3),heights=c(9,2),widths=c(3,3,2))
+g4 <- grid.arrange(g0,g1,g2,g3,layout_matrix=matrix(c(2,3,1,2,4,4),byrow=T,ncol=3),heights=c(9,2),widths=c(3,3,2.5))
 
+g3 <- ggplot(x[active & Phases=="PHASE3"]) + trial.plot + facet_wrap(~ "Phase III") + 
+  geom_text(aes(label=sprintf("%s (%s)",Drug.Name,Acronym),x=last),hjust=0,nudge_x = 60) +
+  scale_color_manual("Study Status",values=my.palette$study.status[3:7],drop=F) +
+  theme(legend.position = "bottom") +
+  guides(colour = guide_legend(direction = "vertical"))
+
+g5 <- grid.arrange(g1,g2,g3,layout_matrix=matrix(c(1,2,1,3),byrow=T,ncol=2),heights=c(10,3))
+
+ggsave(filename = "output/ClinicalTrials.tiff",plot = g4,width = 18,height = 10,dpi=200)
+ggsave(filename = "output/ClinicalTrials.png",plot = g4,width = 18,height = 10,dpi=200)
+ggsave(filename = "output/ClinicalTrials.pdf",plot = g4,width = 18,height = 10)
+
+ggsave(filename = "output/ClinicalTrials2.tiff",plot = g5,width = 12,height = 14,dpi=200)
+ggsave(filename = "output/ClinicalTrials2.png",plot = g5,width = 12,height = 14,dpi=200)
+ggsave(filename = "output/ClinicalTrials2.pdf",plot = g5,width = 12,height = 14)
 
 
---
+++ source/pubchem.R
@@ -0,0 +1,36 @@
+lapply(dir("additional_files/",pattern="PubChem*"),
+       function(x) {
+         d0 <- fread(paste0("additional_files/",x))
+         d0[,File:=x]
+         return(d0)
+       }) %>% rbindlist() -> PubChem
+
+# Remove duplicated entries
+
+PubChem <- PubChem[!duplicated(`Compound CID`)]
+PubChem[,Synonyms:=paste(Name,Synonyms,sep="|"),by=`Compound CID`]
+
+# cross with crc clinical trials
+
+escapeALL <- function(x) {
+  gsub("(\\(|\\)|\\[|\\]|\\{|\\})", ".", x,fixed = F)
+}
+
+shortSynonyms <- function(x) {
+  x <- strsplit(x,split="\\|") %>% unlist
+  x <- x[nchar(x) < 25 & nchar(x) > 4]
+  x2 <- gsub("[-]+","",x)
+  
+  paste(unique(toupper(c(x,x2))),collapse="|")
+}
+
+PubChem[,Synonyms2 := Synonyms %>% escapeALL %>% shortSynonyms,by=`Compound CID`]
+PubChem[,CTrials := crc[grep(Synonyms2,Interventions,ignore.case = T),paste(`NCT Number`,collapse="|")],by=`Compound CID`]
+PubChem[,NTrials := strsplit(CTrials,split="\\|") %>% unlist %>% length,by=`Compound CID`]
+
+PubChem[NTrials>=20] # "carbon
+PubChem <- PubChem[NTrials < 20]
+
+fwrite(PubChem,file="additional_files/PubChem.csv")
+
+
